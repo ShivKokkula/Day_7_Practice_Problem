90d8f3d9c8578e173ccb786e12d17674f6eff1da
ShivKokkula <shivbk987@gmail.com>
2021-07-07 23:37:39 +0530

    Adding all files

--- main/java/AddressBookMain.java
+++ main/java/AddressBookMain.java
@@ -1,2 +1,5 @@
-package PACKAGE_NAME;public class AddressBookMain {
+public class AddressBookMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Address Book System");
+    }
 }
