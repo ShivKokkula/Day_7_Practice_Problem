9c6917131880e514020bc0bda614b15b6861ac51
ShivKokkula <shivbk987@gmail.com>
2021-07-10 00:52:28 +0530

    Refactor - search and show feature

--- main/java/AddressBookMain.java
+++ main/java/AddressBookMain.java
@@ -12,7 +12,7 @@ public class AddressBookMain {
             Menu();
             System.out.println("Enter your selection");
             int selection = input.nextInt();
-            if(selection < 1 || selection > 7) {
+            if(selection < 1 || selection > 6) {
                 System.out.println("Please enter the number in the menu");
                 continue;
             }
@@ -22,16 +22,16 @@ public class AddressBookMain {
                     add();
                     break;
                 case 2:
-                    del();
+                    delete();
                     break;
                 case 3:
                     upDate();
                     break;
                 case 4:
-                    look();
+                    search();
                     break;
                 case 5:
-                    lookAll();
+                    showAll();
                     break;
                 case 6:
                     exit();
@@ -56,56 +56,75 @@ public class AddressBookMain {
 
     private static void exit() {
         System.out.println("Invalid selection");
+        System.exit(1);
     }
 
-    private static void lookAll() {
-        System.out.println("LookAll operation");
+    private static void showAll() {
+        System.out.println("------Query all contacts------");
+        for(int i = 0; i < current_contacts_counts; i++) {
+            System.out.println(contacts[i]);
+        }
     }
 
-    private static void look() {
-        System.out.println("Update operation");
+    private static void search() {
+        System.out.println("------Query Contact------");
+        System.out.println("Please enter the contact first name");
+        String name = input.next();
+        int index = getIndex(name);
+        if(-1 == index) {
+            System.out.println("Contact does not exist");
+        }else {
+            System.out.println("First name: "+contacts[index].getFirstName());
+            System.out.println("Last name: "+contacts[index].getLastName());
+            System.out.println("Address："+contacts[index].getAddress());
+            System.out.println("City："+contacts[index].getCity());
+            System.out.println("State: "+contacts[index].getState());
+            System.out.println("Zip code: "+contacts[index].getZipCode());
+            System.out.println("Phone number: "+contacts[index].getPhoneNumber());
+            System.out.println("Email: "+contacts[index].getEmail());
+        }
     }
 
     private static void upDate() {
-        System.out.println("------Modify contact information------");
+        System.out.println("------Update contact information------");
         System.out.println("Please enter the contact first name");
         String name=input.next();
         int index = getIndex(name);
         if(-1 == index) {
             System.out.println("Contact does not exist");
         }else {
-            System.out.println("Name:"+contacts[index].getFirstName()+"Please enter a new contact first name");
+            System.out.println("First name: "+contacts[index].getFirstName()+" Please enter a new contact first name");
             String firstName = input.next();
             contacts[index].setFirstName(firstName);
-            System.out.println("Name:"+contacts[index].getLastName()+"Please enter a new contact full name");
+            System.out.println("Last name: "+contacts[index].getLastName()+" Please enter a new contact last name");
             String lastName = input.next();
             contacts[index].setLastName(lastName);
-            System.out.println("Name:"+contacts[index].getAddress()+"Please enter the new contact address");
+            System.out.println("Address: "+contacts[index].getAddress()+" Please enter the new contact address");
             String address = input.next();
             contacts[index].setAddress(address);
-            System.out.println("Name:"+contacts[index].getCity()+"Please enter a new contact city");
+            System.out.println("City: "+contacts[index].getCity()+" Please enter a new contact city");
             String city = input.next();
             contacts[index].setCity(city);
-            System.out.println("Name:"+contacts[index].getState()+"Please enter a new contact state");
+            System.out.println("State: "+contacts[index].getState()+" Please enter a new contact state");
             String state = input.next();
             contacts[index].setState(state);
-            System.out.println("Name:"+contacts[index].getZipCode()+"Please enter a new contact zip code");
+            System.out.println("Zip code: "+contacts[index].getZipCode()+" Please enter a new contact zip code");
             String zipCode = input.next();
             contacts[index].setZipCode(zipCode);
-            System.out.println("Name:"+contacts[index].getPhoneNumber()+"Please enter a new contact phone number");
+            System.out.println("Phone number: "+contacts[index].getPhoneNumber()+" Please enter a new contact phone number");
             String phoneNumber = input.next();
             contacts[index].setPhoneNumber(phoneNumber);
-            System.out.println("Name:"+contacts[index].getEmail()+"Please enter a new contact phone number");
+            System.out.println("Email: "+contacts[index].getEmail()+" Please enter a new contact email");
             String email = input.next();
             contacts[index].setEmail(email);
 
         }
     }
 
-    private static void del() {
+    private static void delete() {
         System.out.println("------Delete a contact------");
         System.out.println("Please enter the contact first name");
-        String name=input.next();
+        String name = input.next();
         int index = getIndex(name);
         if(-1 == index) {
             System.out.println("Contact does not exist");
@@ -138,15 +157,15 @@ public class AddressBookMain {
         contacts[current_contacts_counts] = new Contacts(firstName, lastName, address, city, state, zipCode,
                 phoneNumber, email);
 
-        System.out.println(contacts[0].toString());
+        System.out.println(contacts[current_contacts_counts].toString());
+        current_contacts_counts++;
     }
 
     public static int getIndex(String firstName) {
-        for(int i = 0; i < current_contacts_counts; i++) {
-            if(contacts[i].getFirstName().equals(firstName)) {
+        for(int i = 0; i < current_contacts_counts; i++)
+            if (contacts[i].getFirstName().equals(firstName)) {
                 return i;
             }
-        }
         return -1;
     }
 }
